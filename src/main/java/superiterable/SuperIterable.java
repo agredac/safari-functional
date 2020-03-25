2996fb716875927b16b793036a746971663d7472
Simon Roberts <simon@dancingcloudservices.com>
2020-03-25 11:59:16 -0600

    Day 1 Final

--- src/main/java/superiterable/SuperIterable.java
+++ src/main/java/superiterable/SuperIterable.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class SuperIterable<E> implements Iterable<E> {
@@ -13,10 +14,28 @@ public class SuperIterable<E> implements Iterable<E> {
     this.self = self;
   }
 
-  public void forEvery(Consumer<E> op) {
+//  Java 8 Iterable already has "forEach"
+//  public void forEvery(Consumer<E> op) {
+//    for (E e : self) {
+//      op.accept(e);
+//    }
+//  }
+
+  public <F> SuperIterable<F> map(Function<E, F> op) {
+    List<F> res = new ArrayList<>();
+    self.forEach(e -> res.add(op.apply(e)));
+    return new SuperIterable<>(res);
+  }
+
+  public <F> SuperIterable<F> flatMap(Function<E, SuperIterable<F>> op) {
+    List<F> res = new ArrayList<>();
     for (E e : self) {
-      op.accept(e);
+      SuperIterable<F> sif = op.apply(e);
+      for (F f : sif) {
+        res.add(f);
+      }
     }
+    return new SuperIterable<>(res);
   }
 
   public SuperIterable<E> filter(Predicate<E> pred) {
--- src/main/java/superiterable/UseSuperIterable.java
+++ src/main/java/superiterable/UseSuperIterable.java
@@ -4,8 +4,16 @@ import builtins.student.Student;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class UseSuperIterable {
+  public static <E, F> void complexProcess(List<E> data,
+      Predicate<E> p, Function<E, F> f, Consumer<F> c) {
+    //...
+  }
+
   public static void main(String[] args) {
     List<Student> roster = Arrays.asList(
         Student.of("Fred", 3.2, "Math", "Physics"),
@@ -23,6 +31,11 @@ public class UseSuperIterable {
 
     rosterIter
         .filter(s -> s.getGpa() > 3)
-        .forEvery(s -> System.out.println("> " + s));
+        .map(s -> s.getName() + " has grade " + s.getGpa())
+        .forEach(s -> System.out.println("> " + s));
+
+    rosterIter
+        .flatMap(s -> new SuperIterable(s.getCourses()))
+        .forEach(s -> System.out.println(s));
   }
 }
